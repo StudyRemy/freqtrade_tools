908b332ebcc6642c12413f41028a8664763ccba3
StudyRemy <study@heerebout.net>
2021-10-13 09:12:13 +0200

    dynamically create blacklist filename w/ strategy

--- dynamic_blacklist_creator/create_blacklist.py
+++ dynamic_blacklist_creator/create_blacklist.py
@@ -11,15 +11,23 @@ blacklist_template_second = second.string
 # Get blacklist coins
 with open(os.path.join(sys.path[0], 'to_blacklist.txt'), 'r') as file:
     coinlist = file.read()
+    
+# Get strategy name
+with open(os.path.join(sys.path[0], 'stratname.txt'), 'r') as file:
+    strategy = file.read()
 
-# Create blacklist-string
+# Create blacklist string
 string_to_file = blacklist_template_first + coinlist + blacklist_template_second
+
+# Creat blacklist filename
+filename = 'blacklist-' + strategy + '.json'
 
 # Write blacklist to file_object
 # TODO: Create filename dynamically (eg. exchange/currency/strategy)
-blacklist_file = open(os.path.join(sys.path[0], 'a_new_blacklist.json'), 'w')
+blacklist_file = open(os.path.join(sys.path[0], filename), 'w')
 blacklist_file.write(string_to_file)
 blacklist_file.close()
 
-# empty to_blacklist.txt
+# empty to_blacklist.txt and stratname.txt
 open(os.path.join(sys.path[0], 'to_blacklist.txt'), 'w').close()
+open(os.path.join(sys.path[0], 'stratname.txt'), 'w').close()
