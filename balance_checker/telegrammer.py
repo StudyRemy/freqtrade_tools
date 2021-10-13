5fc6e30f91dad4fa8f24a3f3d23828325390ad8f
StudyRemy <study@heerebout.net>
2021-10-13 22:38:18 +0200

    Slight update to hardcoded date

--- balance_checker/telegrammer.py
+++ balance_checker/telegrammer.py
@@ -12,17 +12,17 @@ import datetime
 def get_balance_binance(update: Update, context: CallbackContext) -> None:
     value = checker.check_balance_binance()
     percent = stats.calculate_percent(value, 'binance')
-    update.message.reply_text(f'Your Binance portfolio is worth ${value}\n The difference since 8-9-\'21 is {percent}%')
+    update.message.reply_text(f'Your Binance portfolio is worth ${value}\n The difference since 13-10-\'21 is {percent}%')
 
 def get_balance_kucoin(update: Update, context: CallbackContext) -> None:
     value = checker.check_balance_kucoin()
     percent = stats.calculate_percent(value, 'kucoin')
-    update.message.reply_text(f'Your Kucoin portfolio is worth ${value}\n The difference since 8-9-\'21 is {percent}%')
+    update.message.reply_text(f'Your Kucoin portfolio is worth ${value}\n The difference since 13-10-\'21 is {percent}%')
 
 def get_balance_huobi(update: Update, context: CallbackContext) -> None:
     value = checker.check_balance_huobi()
     percent = stats.calculate_percent(value, 'huobi')
-    update.message.reply_text(f'Your Huobi portfolio is worth ${value}\n The difference since 8-9-\'21 is {percent}%')
+    update.message.reply_text(f'Your Huobi portfolio is worth ${value}\n The difference since 13-10-\'21 is {percent}%')
 
 def get_balance_total(update: Update, context: CallbackContext) -> None:
     value_BN = checker.check_balance_binance()
@@ -35,28 +35,28 @@ def get_balance_total(update: Update, context: CallbackContext) -> None:
     percent_tot = stats.calculate_percent(value_tot, 'total')
     BTC_perc = stats.calculate_BTC_diff(checker.check_BTC_value())
     diff = round(percent - BTC_perc, 2)
-    update.message.reply_text(f'Your total portfolio is worth ${value}\nThe difference since 8-9-\'21 is {percent}% \n BTC has changed {BTC_perc}% \n The difference between portfolio change and market change is {diff}%')
+    update.message.reply_text(f'Your total portfolio is worth ${value}\nThe difference since 13-10-\'21 is {percent}% \n BTC has changed {BTC_perc}% \n The difference between portfolio change and market change is {diff}%')
 
 # TODO: Create message-cretion function. Remove hardcoded strings
 def get_balance_alltime(update: Update, context: CallbackContext) -> None:
     value_BN = checker.check_balance_binance()
-    value_BN_old = stats.get_value('binance', '2021-09-09')
+    value_BN_old = stats.get_value('binance', '2021-10-13')
     percent_BN = round((value_BN/value_BN_old - 1)*100, 2)
     value_KC = checker.check_balance_kucoin()
-    value_KC_old = stats.get_value('kucoin', '2021-09-09')
+    value_KC_old = stats.get_value('kucoin', '2021-10-13')
     percent_KC = round((value_KC/value_KC_old - 1)*100, 2)
     value_HB = checker.check_balance_huobi()
-    value_HB_old = stats.get_value('huobi', '2021-09-09')
+    value_HB_old = stats.get_value('huobi', '2021-10-13')
     percent_HB = round((value_HB/value_HB_old - 1)*100, 2)
     value_tot = value_BN + value_KC + value_HB
-    value_tot_old = stats.get_value('total', '2021-09-09')
+    value_tot_old = stats.get_value('total', '2021-10-13')
     percent_tot = round((value_tot/value_tot_old - 1)*100, 2)
     value_btc = checker.check_BTC_value()
-    value_btc_old = stats.get_value('BTC', '2021-09-09')
+    value_btc_old = stats.get_value('BTC', '2021-10-13')
     percent_btc = round((value_btc/value_btc_old - 1)*100, 2)
     diff = percent_tot - percent_btc
     update.message.reply_text(f'<pre>    ==================================== \n\
-     Exchange| 9-9-\'21 |  now  | % diff  \n\
+     Exchange| 10-13-\'21 |  now  | % diff  \n\
     ---------+---------+-------+-------- \n\
      Binance | ${value_BN_old}    | ${value_BN}  | {"{:.2f}".format(percent_BN)}%   \n\
      Kucoin  | ${value_KC_old}     | ${value_KC}   | {"{:.2f}".format(percent_KC)}%   \n\
@@ -68,7 +68,6 @@ def get_balance_alltime(update: Update, context: CallbackContext) -> None:
     ==================================== \n\n\
     Difference between bots and market is {"{:.2f}".format(diff)}% </pre>', parse_mode=ParseMode.HTML)
 
-# TODO: Create message-cretion function. Remove hardcoded strings
 def get_balance_today(update: Update, context: CallbackContext) -> None:
     today = datetime.date.today().strftime("%Y-%m-%d")
 
